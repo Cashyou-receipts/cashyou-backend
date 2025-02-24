07c5cf9d938cdfb4f84c3626fd9fdf8d50f69e2a
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2025-02-23 17:03:38 -0700

    receipts#create tests happy path

--- spec/requests/receipts_spec.rb
+++ spec/requests/receipts_spec.rb
@@ -16,8 +16,8 @@ RSpec.describe "/receipts", type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Receipt. As you add validations to Receipt, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+  let(:valid_receipt_data) {
+    JSON.parse(File.read("spec/fixtures/json_responses/soops_1.json"))
   }
 
   let(:invalid_attributes) {
@@ -32,96 +32,112 @@ RSpec.describe "/receipts", type: :request do
     {}
   }
 
-  describe "GET /index" do
-    it "renders a successful response" do
-      Receipt.create! valid_attributes
-      get receipts_url, headers: valid_headers, as: :json
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /show" do
-    it "renders a successful response" do
-      receipt = Receipt.create! valid_attributes
-      get receipt_url(receipt), as: :json
-      expect(response).to be_successful
-    end
-  end
-
+  # describe "GET /index" do
+  #   it "renders a successful response" do
+  #     allow_any_instance_of(ReceiptsController).to receive(:verify_request).and_return(nil)
+  #     Receipt.create! valid_attributes
+  #     get receipts_url, headers: valid_headers, as: :json
+  #     expect(response).to be_successful
+  #   end
+  # end
+  
+  # describe "GET /show" do
+  #   it "renders a successful response" do
+  #     receipt = Receipt.create! valid_attributes
+  #     get receipt_url(receipt), as: :json
+  #     expect(response).to be_successful
+  #   end
+  # end
+  
   describe "POST /create" do
     context "with valid parameters" do
+      
       it "creates a new Receipt" do
+        allow_any_instance_of(ReceiptsController).to receive(:verify_request).and_return(nil)
         expect {
           post receipts_url,
-               params: { receipt: valid_attributes }, headers: valid_headers, as: :json
+          params: { receipt: valid_receipt_data }, as: :json
         }.to change(Receipt, :count).by(1)
       end
-
+      
       it "renders a JSON response with the new receipt" do
+        allow_any_instance_of(ReceiptsController).to receive(:verify_request).and_return(nil)
         post receipts_url,
-             params: { receipt: valid_attributes }, headers: valid_headers, as: :json
+        params: { receipt: valid_receipt_data }, as: :json
         expect(response).to have_http_status(:created)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
-    end
-
-    context "with invalid parameters" do
-      it "does not create a new Receipt" do
-        expect {
-          post receipts_url,
-               params: { receipt: invalid_attributes }, as: :json
-        }.to change(Receipt, :count).by(0)
-      end
-
-      it "renders a JSON response with errors for the new receipt" do
+      
+      it "creates a new receipt with the correct attributes" do
+        allow_any_instance_of(ReceiptsController).to receive(:verify_request).and_return(nil)
         post receipts_url,
-             params: { receipt: invalid_attributes }, headers: valid_headers, as: :json
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        params: { receipt: valid_receipt_data }, as: :json
+        created_receipt = Receipt.find JSON.parse(response.body)["receipt"]["id"]
+        expect(created_receipt.balance).to eq(5.99)
+        expect(created_receipt.subtotal).to be_nil
+        expect(created_receipt.tax).to eq(0.22)
+        expect(created_receipt.items.size).to eq(5)
       end
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  #   context "with invalid parameters" do
+  #     it "does not create a new Receipt" do
+  #       expect {
+  #         post receipts_url,
+  #              params: { receipt: invalid_attributes }, as: :json
+  #       }.to change(Receipt, :count).by(0)
+  #     end
 
-      it "updates the requested receipt" do
-        receipt = Receipt.create! valid_attributes
-        patch receipt_url(receipt),
-              params: { receipt: new_attributes }, headers: valid_headers, as: :json
-        receipt.reload
-        skip("Add assertions for updated state")
-      end
+  #     it "renders a JSON response with errors for the new receipt" do
+  #       post receipts_url,
+  #            params: { receipt: invalid_attributes }, headers: valid_headers, as: :json
+  #       expect(response).to have_http_status(:unprocessable_entity)
+  #       expect(response.content_type).to match(a_string_including("application/json"))
+  #     end
+  #   end
+  # end
 
-      it "renders a JSON response with the receipt" do
-        receipt = Receipt.create! valid_attributes
-        patch receipt_url(receipt),
-              params: { receipt: new_attributes }, headers: valid_headers, as: :json
-        expect(response).to have_http_status(:ok)
-        expect(response.content_type).to match(a_string_including("application/json"))
-      end
-    end
+  # describe "PATCH /update" do
+  #   context "with valid parameters" do
+  #     let(:new_attributes) {
+  #       skip("Add a hash of attributes valid for your model")
+  #     }
 
-    context "with invalid parameters" do
-      it "renders a JSON response with errors for the receipt" do
-        receipt = Receipt.create! valid_attributes
-        patch receipt_url(receipt),
-              params: { receipt: invalid_attributes }, headers: valid_headers, as: :json
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
-      end
-    end
-  end
+  #     it "updates the requested receipt" do
+  #       receipt = Receipt.create! valid_attributes
+  #       patch receipt_url(receipt),
+  #             params: { receipt: new_attributes }, headers: valid_headers, as: :json
+  #       receipt.reload
+  #       skip("Add assertions for updated state")
+  #     end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested receipt" do
-      receipt = Receipt.create! valid_attributes
-      expect {
-        delete receipt_url(receipt), headers: valid_headers, as: :json
-      }.to change(Receipt, :count).by(-1)
-    end
-  end
+  #     it "renders a JSON response with the receipt" do
+  #       receipt = Receipt.create! valid_attributes
+  #       patch receipt_url(receipt),
+  #             params: { receipt: new_attributes }, headers: valid_headers, as: :json
+  #       expect(response).to have_http_status(:ok)
+  #       expect(response.content_type).to match(a_string_including("application/json"))
+  #     end
+  #   end
+
+  #   context "with invalid parameters" do
+  #     it "renders a JSON response with errors for the receipt" do
+  #       receipt = Receipt.create! valid_attributes
+  #       patch receipt_url(receipt),
+  #             params: { receipt: invalid_attributes }, headers: valid_headers, as: :json
+  #       expect(response).to have_http_status(:unprocessable_entity)
+  #       expect(response.content_type).to match(a_string_including("application/json"))
+  #     end
+  #   end
+  # end
+
+  # describe "DELETE /destroy" do
+  #   it "destroys the requested receipt" do
+  #     receipt = Receipt.create! valid_attributes
+  #     expect {
+  #       delete receipt_url(receipt), headers: valid_headers, as: :json
+  #     }.to change(Receipt, :count).by(-1)
+  #   end
+  # end
 end
