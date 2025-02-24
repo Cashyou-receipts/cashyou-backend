09588c2d8375f7dcae580bcdbb3202a97cc0a04d
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2025-02-23 17:03:57 -0700

    receipts controller and routes

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,24 +1,29 @@
 class ReceiptsController < ApplicationController
-  before_action: :verify_request
+  before_action :verify_request
 
   def create
     user = User.find_or_create_by(phone_number: params[:phone_number])
-    receipt_data = params[:receipt]
-    receipt_info = ReceiptDissector.new(receipt_data)
-    receipt_info.parse_receipt
-    first_section = receipt_info.first_section_text
-    receipt_vendor = find_vendor_name(user, first_section)
+    receipt_data = params[:receipt].as_json
+    parser = ReceiptDissector.new(receipt_data)
+    parser.parse_receipt
+    receipt_vendor = find_vendor_name(user, params[:receipt][:responses].first[:textAnnotations].first[:description].gsub("\n", " ").downcase)
     receipt = user.receipts.new(
       vendor: receipt_vendor,
-      subtotal: receipt_info.subtotal,
-      tax: receipt_info.tax,
-      balance: receipt_info.balance
+      subtotal: parser.subtotal,
+      tax: parser.tax,
+      balance: parser.balance
     )
+    if receipt.save
+      render json: { receipt: receipt }, status: :created
+    else
+      render json: { errors: receipt.errors.full_messages }, status: :unprocessable_entity
+    end
   end
   
   private
   
   def verify_request
+    # TODO: OAuth
     auth = request.headers["lambda_auth"]
     unless auth && ActiveSupport::SecurityUtils.secure_compare(auth, ENV["lambda_auth"])
       render json: {error: "Unauthorized entity"}, status: :unauthorized
@@ -27,9 +32,9 @@ class ReceiptsController < ApplicationController
   
   def find_vendor_name(user, text)
     text.downcase!
-    vendor_names = user.vendors.pluck(:name)
+    vendors = user.vendors
 
-    existing_vendor = vendor_names.detect { |name| text.match?(name) }
+    existing_vendor = vendors.detect { |vendor| text.match(vendor.name) }
 
     if existing_vendor.present?
       return existing_vendor
